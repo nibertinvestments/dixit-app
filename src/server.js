6d9a9a2c5c775c75e0d82b2c424614fb5616dd78
dkarrieta71 <castanoalex526@gmail.com>
2025-07-31 16:52:35 -0700

    sleep added

--- src/server.js
+++ src/server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World from EKS CI/CD Demo!');
 });
 
+app.get('/sleep', (req, res) => {
+  const ms = parseInt(req.query.ms) || 1000;
+  setTimeout(() => res.send(`Slept ${ms}ms`), ms);
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
